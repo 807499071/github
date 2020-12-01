d24799938ec7f39dd7869b4047038fea5a888d41
dong <807499071@qq.com>
2020-12-01 15:35:27 +0800

    test01

---
+++ dong/src/com/dong/test01.java
@@ -0,0 +1,9 @@
+package com.dong;
+
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        int i = 1;
+        System.out.println(2);
+    }
+}
